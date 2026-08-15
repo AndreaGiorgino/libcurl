26d52777c3e3d95833af7bf8813b675fb8aeb95e
giorgi.page <andreagiorgino1@gmail.com>
2026-08-15 19:31:57 +0200

    response base class defined

---
+++ include/libcurl/response.hxx
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <libcurl/headers.hxx>
+#include <libcurl/status_codes.hxx>
+#include <unordered_map>
+#include <variant>
+
+namespace libcurl {
+/**
+ * @class response
+ * @brief Represents an http response
+ *
+ */
+class response final {
+   public: // ctors
+    response(void) noexcept = default;
+
+    response(const response&) noexcept                     = default;
+    auto operator =(const response&) noexcept -> response& = default;
+
+    response(response&&) noexcept                     = default;
+    auto operator =(response&&) noexcept -> response& = default;
+
+    ~response(void) noexcept = default;
+};
+} // namespace libcurl
