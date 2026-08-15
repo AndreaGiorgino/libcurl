a2a6be1a123efd902de55fe4bb6fe56f1ad6d7f3
giorgi.page <andreagiorgino1@gmail.com>
2026-08-15 19:27:42 +0200

    request lookup methods implemented

--- src/libcurl/request.cxx
+++ src/libcurl/request.cxx
@@ -10,6 +10,38 @@ request::request(std::string_view url, methods method,
     setHeaders(headers);
 }
 
+auto request::getUrl(void) const noexcept -> std::string {
+    return _url;
+}
+
+auto request::getMethod(void) const noexcept -> methods {
+    return _method;
+}
+
+auto request::getHeader(
+    std::variant<headers, std::string_view> header) const noexcept
+    -> std::string {
+    const auto name {std::visit(
+        [](const auto& header) -> std::string {
+            using clean_t = std::remove_cvref_t<decltype(header)>;
+
+            if constexpr (std::same_as<clean_t, headers>)
+                return getHeaderName(header);
+            else
+                return header.data();
+        },
+        header)};
+
+    if (const auto it {_headers.find(name)}; it != _headers.end())
+        return it->second;
+    return {};
+}
+
+auto request::getHeaders(void) const noexcept
+    -> std::unordered_map<std::string, std::string> {
+    return _headers;
+}
+
 auto request::setUrl(std::string_view url) noexcept -> void {
     _url = url;
 }
@@ -38,36 +70,5 @@ auto request::setHeaders(std::initializer_list<
     std::pair<std::variant<headers, std::string_view>, std::string_view>>
         headers) noexcept -> void {
     for (const auto& [header, value] : headers) setHeader(header, value);
-}
-
-auto request::getUrl(void) const noexcept -> std::string {
-    return _url;
-}
-
-auto request::getMethod(void) const noexcept -> methods {
-    return _method;
-}
-
-auto request::getHeader(
-    std::variant<headers, std::string_view> header) const noexcept
-    -> std::string {
-    const auto name {std::visit(
-        [](const auto& header) -> std::string {
-            using clean_t = std::remove_cvref_t<decltype(header)>;
-
-            if constexpr (std::same_as<clean_t, headers>)
-                return getHeaderName(header);
-            else
-                return header.data();
-        },
-        header)};
-
-    if (const auto it {_headers.find(name)}; it != _headers.end()) return it->second;
-    return {};
-}
-
-auto request::getHeaders(void) const noexcept
-    -> std::unordered_map<std::string, std::string> {
-    return _headers;
 }
 } // namespace libcurl
