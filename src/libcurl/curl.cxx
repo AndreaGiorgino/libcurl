788ae2bfec7be2385151d10f926cc68344717712
giorgi.page <andreagiorgino1@gmail.com>
2026-08-17 15:06:35 +0200

    strip utilities implemented

--- src/libcurl/curl.cxx
+++ src/libcurl/curl.cxx
@@ -1,4 +1,23 @@
-#include "libcurl/curl.hxx"
+#include <curl/curl.h>
+
+#include <cstring>
+#include <format>
+#include <libcurl/curl.hxx>
 
 namespace libcurl {
+[[nodiscard]] auto lstrip(std::string_view str) noexcept -> std::string {
+    return {std::find_if(
+        str.cbegin(), str.cend(), [](auto ch) { return !std::isspace(ch); })};
 }
+
+[[nodiscard]] auto rstrip(std::string_view str) noexcept -> std::string {
+    return {str.begin(), std::find_if(str.crbegin(), str.crend(), [](auto ch) {
+                return !std::isspace(ch);
+            }).base()};
+}
+
+[[nodiscard]] auto strip(std::string_view str) noexcept -> std::string {
+    return lstrip(rstrip(str));
+}
+
+} // namespace libcurl
