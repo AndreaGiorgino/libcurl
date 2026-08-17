8e178c9801d0bf8f8876a22f4d3e1a81cd433a5b
giorgi.page <andreagiorgino1@gmail.com>
2026-08-17 15:06:59 +0200

    write body callback implemented

--- src/libcurl/curl.cxx
+++ src/libcurl/curl.cxx
@@ -20,4 +20,28 @@ namespace libcurl {
     return lstrip(rstrip(str));
 }
 
+struct mem_buffer {
+    char* data {(char*)malloc(1)};
+    size_t size {};
+};
+
+auto writeBodyCallback(void* contents, std::size_t size, std::size_t nmemb,
+    void* userp) -> std::size_t {
+    const auto realsize {size * nmemb};
+
+    auto* mem {(mem_buffer*)userp};
+    auto* ptr {(char*)realloc(mem->data, mem->size + realsize + 1)};
+
+    // check for out of memory
+    if (!ptr) return 0;
+
+    mem->data = ptr;
+    std::memcpy(&(mem->data[mem->size]), contents, realsize);
+    mem->size += realsize;
+
+    // NULL terminated string
+    mem->data[mem->size] = 0;
+
+    return realsize;
+}
 } // namespace libcurl
