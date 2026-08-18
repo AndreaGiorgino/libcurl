06f39b9f38cf52d9b846a1d6de553233bc0c7e2d
giorgi.page <andreagiorgino1@gmail.com>
2026-08-18 17:53:32 +0200

    test methods get name implemented

---
+++ CTest/TestMethods_GetName.cxx
@@ -0,0 +1,22 @@
+#include <helpers.hxx>
+#include <libcurl/methods.hxx>
+
+using libcurl::methods;
+
+auto TestMethods_GetName(int, char**) -> int {
+    helpers::checkeq(libcurl::getMethodName(methods::DELETE), "DELETE");
+    helpers::checkeq(libcurl::getMethodName(methods::GET), "GET");
+    helpers::checkeq(libcurl::getMethodName(methods::HEAD), "HEAD");
+    helpers::checkeq(libcurl::getMethodName(methods::OPTIONS), "OPTIONS");
+    helpers::checkeq(libcurl::getMethodName(methods::PATCH), "PATCH");
+    helpers::checkeq(libcurl::getMethodName(methods::POST), "POST");
+    helpers::checkeq(libcurl::getMethodName(methods::PUT), "PUT");
+    helpers::checkeq(libcurl::getMethodName(methods::TRACE), "TRACE");
+
+    try {
+        (void)libcurl::getMethodName((methods)(1 << 30));
+        return 1;
+    } catch (...) {
+        return 0;
+    }
+}
